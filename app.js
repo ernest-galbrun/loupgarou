fdee5167eea6692f661f9e98037860c91dd90d3e
Neptune1111 <51018949+Neptune1111@users.noreply.github.com>
2020-03-05 18:32:32 +0100

    add discord dependencies and discord first bot

--- app.js
+++ app.js
@@ -21,3 +21,56 @@ app.listen(PORT, () => {
 // [END gae_node_request_example]
 
 module.exports = app;
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "/";
+
+client.login("NjY2MjQzMjM3MzEyNDYyODQ5.XjbzfQ.IReMd0aYDx7-97aswW0bpt0YDk4");
+
+client.on("message", message =>{
+    if(!message.guild) return
+    if(message.content === prefix + "start"){
+        message.channel.send(message.author.username + " a lancé une partie de loup garou");
+        addplayer(message.author.username)
+        main();
+    }
+})
+
+
+var liste_joueurs = []
+var liste_pseudos= []
+var nb_joueurs = 0
+
+function addplayer(pseud){
+ let player = {
+    pseudo:pseud,
+    role : "",
+    etat: null
+}
+liste_joueurs.push(player);
+liste_pseudos.push(player.pseudo)
+console.log(liste_joueurs)
+nb_joueurs++
+}
+
+
+/*
+function rolealea(){
+    liste_roles = ["lg","villageois"];
+    return liste_roles[Math.floor(Math.random() * 2)];
+}
+
+let role = message.guild.roles.find('name',"lg")
+        message.member.addRole(role)
+*/
+
+function main(){
+    client.on("message", message =>{
+    if(message.content === prefix + "join" && message.author.username  in liste_pseudos === true ){
+    addplayer(message.author.username)
+
+    message.channel.send(message.author.username +  " a rejoint la partie");
+    message.channel.send(nb_joueurs + " joueurs ont déja rejoint la partie");
+    }})
+
+}
